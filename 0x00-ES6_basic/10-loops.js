e168879e5db42462db61a50948ea94a74194c00e
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-01-05 12:51:24 +0200

    fix: add inline linter ignore

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-undef */
 export default function appendToEachArrayValue(array, appendString) {
   const tmp = [];
   for (const value of array) {
-    tmp.append(appendString + value);
+    tmp.append(appendString + value); // eslint-disable-line no-param-reassign
   }
 
   return tmp;
