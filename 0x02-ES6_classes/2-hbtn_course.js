868b72044dea1c7e95dd1fb89cd87f682a35ff48
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-01-11 18:05:29 +0200

    fix: rename methods

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -12,28 +12,28 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  get _name() {
+  get name() {
     return this._name;
   }
 
-  set _name(name) {
+  set name(name) {
     this._name = name;
   }
 
-  get _length() {
+  get length() {
     return this._length;
   }
 
-  set _length(length) {
+  set length(length) {
     if (typeof length !== "number") throw TypeError("length must be a number");
     this._length = length;
   }
 
-  get _students() {
+  get students() {
     return this._students;
   }
 
-  set _students(students) {
+  set students(students) {
     if (!Array.isArray(students)) throw TypeError("students must be an Array");
     for (const student of students) {
       if (typeof student !== "string")
