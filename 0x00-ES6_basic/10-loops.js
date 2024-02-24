25a18638f91da3a9daca7911418bb15ac7b1c957
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-24 20:18:50 +0200

    fix: use push instead of append

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const tmp = [];
   for (const value of array) {
-    tmp.append(appendString + value); // eslint-disable-line no-param-reassign
+    tmp.push(appendString + value); // eslint-disable-line no-param-reassign
   }
 
   return tmp;
