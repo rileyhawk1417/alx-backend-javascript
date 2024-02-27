563b0053ee380275f0b9a44765b8c533e2f2cbb2
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-27 10:18:51 +0200

    feat: complete tasks for express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const port = 1245;
+const app = express();
+const countStudents = require("./3-read_file_async");
+
+app.get("/", (req, res) => {
+  res.send("Hello Holberton School!").statusCode = 200;
+});
+
+app.get("/students", async (req, res) => {
+  const intro = "This is the list of our students\n";
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${intro}${data.join("\n")}`).statusCode = 200;
+  } catch (err) {
+    res.send(err.message).statusCode = 404;
+  }
+});
+
+app.listen(port, (req, res) => {
+  console.log("Running on localhost:1245");
+});
