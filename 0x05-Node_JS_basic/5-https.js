945bd7c9f81c846c1c817a724b6414fb6cbca9a0
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-27 07:10:02 +0200

    feat: complete task 5

---
+++ 0x05-Node_JS_basic/5-https.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const port = 1245;
+const countStudents = require("./3-read_file_async");
+
+const app = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+});
+
+app.on("request", async (req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Hello Holberton School!");
+  }
+  if (req.url === "/students") {
+    try {
+      res.write("This is the list of our students\n");
+      const data = await countStudents(process.argv[2]);
+      res.statusCode = 200;
+      res.end(`${data.join("\n")}`);
+    } catch (err) {
+      res.statusCode = 404;
+      res.end(err.message);
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
+module.exports = app;
